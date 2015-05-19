a98cdff46e18727c19f088447d1376a42a3c5d9f
rilakuma <richardbowker@monkeygroover.com>
2015-05-19 16:44:15 +0100

    hlist experiment WIP
    Change-Id: Ifd828908b0152d5ba8b8b1bfad29a3ccf2acca1b

--- build.sbt
+++ build.sbt
@@ -4,4 +4,7 @@ version := "1.0"
 
 scalaVersion := "2.11.6"
 
-libraryDependencies += "org.scala-lang.modules" %% "scala-pickling" % "0.10.0"
+libraryDependencies ++= Seq(
+  "org.scala-lang.modules" %% "scala-pickling" % "0.10.0",
+  "com.chuusai" %% "shapeless" % "2.2.0-RC6"
+)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,31 +2,51 @@
  * Created by rilakkuma on 27/04/2015.
  */
 
-import scala.pickling.Defaults._, scala.pickling.binary._
+import shapeless._
+import scala.pickling.Defaults._, scala.pickling.json._
+
+case class DomainPayload(field1: String, field2: List[String])
 
 sealed trait MessageBase
 case class Successful(blah: Map[Int, String]) extends MessageBase
 case class Failed(blah: String) extends MessageBase
 
 object Main extends App {
+//
+//  val a = Successful(Map((1 -> "5"), (2 -> "hello")))
+//  val aOnWire = a.pickle.value
+//
+//  val unpickledA = aOnWire.unpickle[MessageBase]
+//  // over the message bus
+//  process(unpickledA)
+//
+//
+//  val b = Failed("error!!!!!")
+//  val bOnWire = b.pickle.value
+//  // over the message bus
+//  val unpickledB = bOnWire.unpickle[MessageBase]
+//
+//  process(unpickledB)
+//
+//  def process(message: MessageBase) = message match {
+//    case Successful(blah) => println(blah)
+//    case Failed(blah) => println(blah)
+//  }
 
-  val a = Successful(Map((1 -> "5"), (2 -> "hello")))
-  val aOnWire = a.pickle.value
 
-  val unpickledA = aOnWire.unpickle[MessageBase]
-  // over the message bus
-  process(unpickledA)
+  type PayloadIn[T] = T :: HNil
+  type PayloadOut[T] = Int :: Int :: T :: HNil
+
+  val documentIn= DomainPayload("hello", "hello" :: "Hello" :: Nil) :: HNil
+
+  val documentWithId = 10 :: 1 :: documentIn
 
 
-  val b = Failed("error!!!!!")
-  val bOnWire = b.pickle.value
-  // over the message bus
-  val unpickledB = bOnWire.unpickle[MessageBase]
-
-  process(unpickledB)
-
-  def process(message: MessageBase) = message match {
-    case Successful(blah) => println(blah)
-    case Failed(blah) => println(blah)
+  def process(blah: HList) = blah match {
+    case x: PayloadIn[_] => println("in")
+    case id: PayloadOut[_] => println("out")
   }
+
+  process(documentIn)
+  process(documentWithId)
 }
