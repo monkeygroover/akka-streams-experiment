c698ec3a67398b285e41340a61e5fe9973defc86
rilakkuma <richard.bowker@monkeygroover.com>
2015-05-20 00:33:25 +0100

    dsl steps

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,23 +3,27 @@
  */
 
 import akka.actor.ActorSystem
-import akka.stream.ActorFlowMaterializer
+import akka.stream._
 import akka.stream.scaladsl._
-import com.mfglabs.stream.extensions.shapeless.ShapelessStream
+import com.mfglabs.stream.extensions.shapeless._
 import org.scalatest._
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time._
 import shapeless._
 
+
 object Main extends App with Matchers with ScalaFutures {
 
   implicit val as = ActorSystem()
   implicit val fm = ActorFlowMaterializer()
-  implicit override val patienceConfig =
-    PatienceConfig(timeout = Span(5, Minutes), interval = Span(5, Millis))
+  implicit override val patienceConfig = PatienceConfig(timeout = Span(5, Minutes), interval = Span(5, Millis))
+
+  //domain
+  case class Add(data: String)
+  case class Get()
 
   // 1 - Create a type alias for your coproduct
-  type C = Int :+: String :+: Boolean :+: CNil
+  type C = Add :+: Get :+: CNil
 
   // The sink to consume all output data
   val sink = Sink.fold[Seq[C], C](Seq())(_ :+ _)
@@ -27,24 +31,27 @@ object Main extends App with Matchers with ScalaFutures {
   // 2 - a sample source wrapping incoming data in the Coproduct
   val f = FlowGraph.closed(sink) { implicit builder => sink =>
     import FlowGraph.Implicits._
+
     val s = Source(() => Seq(
-      Coproduct[C](1),
-      Coproduct[C]("foo"),
-      Coproduct[C](2),
-      Coproduct[C](false),
-      Coproduct[C]("bar"),
-      Coproduct[C](3),
-      Coproduct[C](true)
+      Coproduct[C](Add("hello")),
+      Coproduct[C](Get())
     ).toIterator)
 
     // 3 - our typed flows
-    val flowInt = Flow[Int].map{i => println("i:"+i); i}
-    val flowString = Flow[String].map{s => println("s:"+s); s}
-    val flowBool = Flow[Boolean].map{s => println("s:"+s); s}
+    val add = Flow[Add].map {
+      a => println(s"stage1 $a"); a}.map {
+      a => println(s"stage2 $a"); a}.map {
+      a => println(s"stage3 $a"); a}
+
+    val get = Flow[Get].map {
+      g => println(s"stage1 $g"); g}.map {
+      g => println(s"stage2 $g"); g}.map {
+      g => println(s"stage3 $g"); g}
+
 
     // >>>>>> THE IMPORTANT THING
     // 4 - build the coproductFlow in a 1-liner
-    val fr = builder.add(ShapelessStream.coproductFlow(flowInt :: flowString :: flowBool :: HNil))
+    val fr = builder.add(ShapelessStream.coproductFlow(add :: get :: HNil))
     // <<<<<< THE IMPORTANT THING
 
     // 5 - plug everything together using akkastream DSL
@@ -53,13 +60,8 @@ object Main extends App with Matchers with ScalaFutures {
   }
 
   // 6 - run it
-  f.run().futureValue.toSet should equal (Set(
-    Coproduct[C](1),
-    Coproduct[C]("foo"),
-    Coproduct[C](2),
-    Coproduct[C](false),
-    Coproduct[C]("bar"),
-    Coproduct[C](3),
-    Coproduct[C](true)
-  ))
+  f.run()
+//    .futureValue.toSet should equal (Set(
+//    Coproduct[C](Add("Hello"))
+//  ))
 }
