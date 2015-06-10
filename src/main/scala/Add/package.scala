973b205509a7c8dee683ca39233f35a7dbd38402
richard bowker <richardbowker@monkeygroover.com>
2015-06-10 09:08:03 +0100

    use flowgraph dsl
    Change-Id: I4640569e5d2755cee501f7ff9520e8c4186630ab

--- src/main/scala/Add/package.scala
+++ src/main/scala/Add/package.scala
@@ -1,11 +1,13 @@
 import Pipeline.{Acceptor, Performer}
 import akka.stream.scaladsl._
 import shapeless.{:+:, CNil, Coproduct}
+import FlowGraph.Implicits._
 
 /**
  * Created by rilakkuma on 21/05/2015.
  */
 package object Add {
+
   //inputs
   case class Add(data: String)
 
@@ -13,7 +15,9 @@ package object Add {
 
   //outputs
   case class Accepted()
+
   case class Success(message: String)
+
   case class Rejected()
 
   // a coproduct of the valid outputs
@@ -24,29 +28,20 @@ package object Add {
 
   def addFlow() = Flow() { implicit builder =>
 
-    val acceptor = builder.add(Acceptor((a:Add) => if (a.data != "Junk") Left(Perform()) else Right(Rejected())))
+    val acceptor = builder.add(Acceptor((a: Add) => if (a.data != "Junk") Left(Perform()) else Right(Rejected())))
 
-    val performer = builder.add(Performer( (a:Perform) => Coproduct[Out](Success("Whoop!")), Coproduct[Out](Accepted())))
+    val performer = builder.add(Performer((a: Perform) => Coproduct[Out](Success("Whoop!")), Coproduct[Out](Accepted())))
 
-    val fRejectPipe = Flow[Rejected].map[Out]{a => Coproduct[Out](Rejected())}
+    val rejectPipe = Flow[Rejected].map[Out] { a => Coproduct[Out](Rejected()) }
 
     val outputMerge = builder.add(Merge[Out](3))
 
-    builder.addEdge(acceptor.rejected, fRejectPipe, outputMerge.in(0))
-    builder.addEdge(acceptor.accepted, performer.in)
+    acceptor.rejected ~> rejectPipe ~> outputMerge
+    acceptor.accepted ~> performer.in
+                         performer.accepted ~> outputMerge
+                         performer.result ~> outputMerge
 
-    builder.addEdge(performer.accepted, outputMerge.in(1))
-    builder.addEdge(performer.result, outputMerge.in(2))
-
-
-    import FlowGraph.Implicits._
-//    accept ~> fRejected ~> merge
-//    accept ~> fPerformed ~> merge
 
     (acceptor.in, outputMerge.out)
   }
-
-
-
-
 }
